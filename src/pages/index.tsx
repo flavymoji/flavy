41a0b5b05c04050baefc379ffc41756128c50e34
ndaemy <yuyaebean@gmail.com>
2022-09-01 23:08:57 +0900

    💄 Install chakra ui and cleanup directory structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Center, Heading } from "@chakra-ui/react";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <Center w="100vw">
+      <Heading>Hello Chakra UI</Heading>
+    </Center>
+  );
+};
+
+export default Home;
