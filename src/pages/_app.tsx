9fa85c3e496b40dbefaeb762f66fc9185bb3b4d0
ndaemy <yuyaebean@gmail.com>
2022-09-08 17:07:19 +0900

    ✨ Create header and footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+
+import Footer from "~/components/Footer";
+import Header from "~/components/Header";
+
 import "normalize.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </ChakraProvider>
   );
 };
