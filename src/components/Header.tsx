8225166894bff121e2f8cc0aed602ac4b12066f7
pinesollee <dayofpine@gmail.com>
2022-09-22 20:50:06 +0900

    ✨ 헤더 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,51 @@
+import {
+  HStack,
+  Text,
+  Button,
+  Menu,
+  MenuList,
+  MenuButton,
+  MenuItem
+} from "@chakra-ui/react";
+import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 import { FC } from "react";
 
 const Header: FC = () => {
-  return <>Header</>;
+  return (
+    <HStack alignItems="center" padding="13px 56px" justifyContent="space-between">
+      <Text>flavy-moji</Text>
+      <HStack>
+        <Menu>
+          {({ isOpen }) => (
+            <>
+              <MenuButton
+                px={4}
+                py={2}
+                transition="all 0.2s"
+                borderRadius="md"
+                borderWidth="1px"
+                size='sm'
+                background='white'
+                _hover={{ bg: "gray.400" }}
+                isActive={isOpen}
+                as={Button}
+                rightIcon={isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+              >
+                한국어
+              </MenuButton>
+              <MenuList>
+                <MenuItem>한국어</MenuItem>
+                <MenuItem>English</MenuItem>
+              </MenuList>
+            </>
+          )}
+        </Menu>
+        <Button colorScheme="gray" size="sm">
+          개발 블로그
+        </Button>
+      </HStack>
+    </HStack>
+  );
 };
 
 export default Header;
