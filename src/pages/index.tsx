c134fdff3ba6177e2a84e1804f4c92f018e23094
pinesollee <dayofpine@gmail.com>
2022-09-27 19:09:55 +0900

    ✨ heading text 및 box 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
-import { Center, Heading } from "@chakra-ui/react";
+import { Box, Center, Heading, HStack, VStack } from "@chakra-ui/react";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <Center w="100vw">
-      <Heading>Hello Chakra UI</Heading>
+      <VStack>
+      <Heading>Make Your Slack Emoticon!</Heading>
+      <text font-Weight='800' margin-top='24px' margin-bottom='30px' fontSize='24px'>이모티콘으로 변환하고 싶은 텍스트를 입력해주세요!</text>
+        <Box>
+          <text>gif 이미지</text>
+        </Box>
+
+        <HStack paddingTop='30px' spacing='16px'>
+          <Box w='467px' height='485px' border='1px' borderColor='gray.200' borderRadius='16px'></Box>
+          <Box w='467px' height='485px' border='1px' borderColor='gray.200' borderRadius='16px'></Box>
+        </HStack>      
+      </VStack>
+
     </Center>
   );
 };
